74c602ee96b4b2b6edd056650d5d0eac266986ff
van der bruggen <vanderbr@devasic126.be.int.net.nokia.com>
2024-08-07 19:42:53 +0200

    Lazy load treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,57 +1,55 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  config = function()
-      require("nvim-treesitter.configs").setup({
-          -- A list of parser names, or "all"
-          ensure_installed = {
-            'python',
-            'lua',
-            'verilog',
-            'json',
-            'yaml',
-            'bash',
-            'dockerfile',
-            'c',
-            'cpp',
-            'make',
-            'regex',
-            'markdown',
-            'rst',
-            'vim',
-            'vimdoc',
-            'query',
-            'typescript',
-            --'tcl',
-            'proto',
-            'matlab',
-            'kotlin',
-          },
+  event = { "VeryLazy" },
+  init = function()
+    -- Should we do something here?
+  end,
+  cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+  opts_extend = { "ensure_installed" },
+  opts = {
+    ensure_installed = {
+      'python',
+      'lua',
+      'verilog',
+      'json',
+      'yaml',
+      'bash',
+      'dockerfile',
+      'c',
+      'cpp',
+      'make',
+      'regex',
+      'markdown',
+      'rst',
+      'vim',
+      'vimdoc',
+      'query',
+      'typescript',
+      --'tcl',
+      'proto',
+      'matlab',
+      'kotlin',
+    },
 
-          -- Install parsers synchronously (only applied to `ensure_installed`)
-          sync_install = false,
+    indent = {
+        enable = true
+    },
 
-          -- Automatically install missing parsers when entering buffer
-          -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
-          auto_install = true,
+    highlight = {
+        enable = true,
 
-          indent = {
-              enable = true
-          },
+        -- Disable for SV/V, avoid rainbow vommit
+        disable = { 'systemverilog', 'verilog', "lua" },
 
-          highlight = {
-              -- `false` will disable the whole extension
-              enable = true,
-
-              -- Disable for SV/V
-              disable = { 'systemverilog', 'verilog' },
-
-              -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-              -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
-              -- Using this option may slow down your editor, and you may see some duplicate highlights.
-              -- Instead of true it can also be a list of languages
-              additional_vim_regex_highlighting = { "markdown" },
-          },
-      })
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = { "markdown" },
+    },
+  },
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
   end
 }
